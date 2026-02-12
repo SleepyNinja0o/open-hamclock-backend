e745c10bec1979ed1dddf60f419fde9ada61d2ab
Dave Koberstein <davek@komacke.com>
2026-02-11 17:16:59 -0800

    Fix aurora initialization issue with data invalid
    The aurora.txt file appears to cause an issue. It seems that while the
    data fills in 1 point every 30 minutes, that the timestamps have a
    discontinuity where seed data from git and the new data meet.
    
    To resolve this, the script notes there is no file and seeds it with the
    latest data for the last 48 values (24 hours). In hamclock the data will
    be a straight line however it will fill in after 24 hours. And of course
    data persists across an upgrade so this only happens for a fresh
    install.
    
    Also removing the other artifacts in the aurora htdocs path. I see they
    exist on CSI but there's no evidence they are used. Elwood must have had
    them laying around.
    
    If we see issue, we can ressurect them.
    
    The rename below is phantom. I deleted the ovation-bad file and created
    an ignore to keep the folder. Since they are both empty git treats them
    as a rename.

--- scripts/gen_aurora.sh
+++ scripts/gen_aurora.sh
@@ -12,10 +12,26 @@ MAX_VALUE=$(curl -s "$URL" | jq '.coordinates | map(.[2]) | max')
 # 2. Get the current UNIX epoch time
 EPOCH_TIME=$(date +%s)
 
-# 3. Append the new data to the file
-echo "$EPOCH_TIME $MAX_VALUE" >> "$OUT"
+# if this is a fresh install, we won't have the history. It seems like
+# hamclock doesn't like old timestamps so we can't keep a seed file in git.
+# Instead what we'll do is take the last value and save it 48 times to meet
+# the hamclock requirement of 48 data points. It will be just a straight line
+# but eventually it will fill in.
+if [ -e "$OUT" ]; then
+    # 3. Append the new data to the file
+    echo "$EPOCH_TIME $MAX_VALUE" >> "$OUT"
 
-# 4. Trim the file to keep only the last 48 lines
-# This keeps the file size constant by slicing off the oldest entry
-TRIMMED_DATA=$(tail -n 48 "$OUT")
-echo "$TRIMMED_DATA" > "$OUT"
+    # 4. Trim the file to keep only the last 48 lines
+    # This keeps the file size constant by slicing off the oldest entry
+    TRIMMED_DATA=$(tail -n 48 "$OUT")
+    echo "$TRIMMED_DATA" > "$OUT"
+
+else
+    # if the file doesn't exist, go backwards for 48 samples every
+    # 30 minutes
+    for i in {0..47}; do
+        echo "$(($EPOCH_TIME - 30 * 60 * $i)) $MAX_VALUE" >> "$OUT"
+    done
+    # needs to be increasing
+    sort -V -o $OUT $OUT
+fi
